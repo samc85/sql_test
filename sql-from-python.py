a81d5263a162d9987bb0be39da74eb5ba38656de
Sam Croyle <sam300385@hotmail.com>
2020-09-26 12:27:36 +0000

    update many lesson

--- sql-from-python.py
+++ sql-from-python.py
@@ -1,7 +1,8 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -12,12 +13,8 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        connection.commit()
 finally:
-    # Close the connection, regardless of success
     connection.close()
